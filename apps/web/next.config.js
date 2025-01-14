c9ea5581d0169920fc5d653e248ef4cf45517c80
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2025-01-14 21:08:19 +0900

    🎉 init

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["@repo/ui"],
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+};
 
 export default nextConfig;
