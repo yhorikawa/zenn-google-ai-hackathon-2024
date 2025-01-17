572ef9500490a7a5446381a45c2c7be28a4e7e2f
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-01-17 17:30:36 +0000

    hono/clientでフェッチできる仕組みを追加

---
+++ apps/web/src/lib/client/client.ts
@@ -0,0 +1,16 @@
+import { hc } from "hono/client";
+import type { AppType } from "../../../../server/src/index";
+import { BACKEND_URL, isBuildMode } from "../../constants/env";
+
+const cache = (cache: RequestInit["cache"]) => {
+  if (!isBuildMode) return "no-store";
+  return cache ? cache : "force-cache";
+};
+
+export const client = (init?: RequestInit) =>
+  hc<AppType>(BACKEND_URL, {
+    init: {
+      ...init,
+      cache: cache(init?.cache),
+    },
+  });
