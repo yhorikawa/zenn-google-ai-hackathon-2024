bc4596713438be7cad2e1c5454dec8e886b0755d
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-01-17 17:30:17 +0000

    env.tsを追加

--- apps/web/src/constants/env.ts
+++ apps/web/src/constants/env.ts
@@ -1,1 +1,8 @@
 //INFO: 環境変数があればここに作って読み込んでください
+export const isBuildMode = process.env.NODE_ENV === "production";
+
+export const BASE_URL = "https://xxxx.com";
+export const APPLICATION_URL =
+  process.env.NEXT_PUBLIC_APPLICATION_URL ?? "http://localhost:3001";
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:3000";
