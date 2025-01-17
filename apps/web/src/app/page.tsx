6139b5b781c388abd297a3fde8dc601fcdbc6bd6
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-01-17 17:31:13 +0000

    サーバーサイドのフェッチができるようのテスト

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,18 @@
+import { client } from "@/lib/client";
 import { Button } from "flowbite-react";
 import { SITE_TITLE } from "../constants/site";
 
-const Page = () => {
+const Page = async () => {
+  const res = await client().index.$get();
+  const data = await res.text();
+
   return (
     <>
       <div className="flex justify-center shadow bg-slate-200 items-center mt-12 py-8 rounded">
         <h1 className="text-7xl font-bold text-on-surface-primary">
           {SITE_TITLE}
         </h1>
+        <p>{data}</p>
       </div>
       <Button>Click me</Button>
     </>
