86b4833600ff20a3b2c809b42363a00f2531dea2
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2025-01-18 20:01:50 +0900

    ✨ add new route for greeting in the Hono app

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,9 +2,14 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-const routes = app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+const routes = app
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  })
+
+  .get("/hello/", (c) => {
+    return c.text("Hello!");
+  });
 
 export default app;
 export type AppType = typeof routes;
