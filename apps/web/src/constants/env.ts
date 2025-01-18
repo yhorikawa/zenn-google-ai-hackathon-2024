0c211299cbb17b7d51e136e0f8cd6811ee52b714
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-01-18 11:12:29 +0000

    環境変数のキーを修正し、NEXT_PUBLIC_BACKEND_URLをBACKEND_URLに変更

--- apps/web/src/constants/env.ts
+++ apps/web/src/constants/env.ts
@@ -7,4 +7,4 @@ export const APPLICATION_URL =
   process.env["NEXT_PUBLIC_APPLICATION_URL"] ?? "http://localhost:3001";
 export const BACKEND_URL =
   // biome-ignore lint/complexity/useLiteralKeys: <explanation>
-  process.env["NEXT_PUBLIC_BACKEND_URL"] ?? "http://localhost:3000";
+  process.env["BACKEND_URL"] ?? "http://localhost:3000";
