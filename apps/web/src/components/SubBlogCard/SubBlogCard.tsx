69e5f112dc2ca5427d5374fe3ff77ff3b392e5d6
oka <76928095+takahiro-okada@users.noreply.github.com>
2025-01-23 22:01:41 +0000

    🚧 Add subblogcard component

---
+++ apps/web/src/components/SubBlogCard/SubBlogCard.tsx
@@ -0,0 +1,29 @@
+type BlogCardProps = {
+  href: string;
+  imgSrc: string;
+  title: string;
+  date: string;
+};
+
+const SubBlogCard: React.FC<BlogCardProps> = ({
+  href,
+  imgSrc,
+  title,
+  date,
+}) => {
+  return (
+    <li>
+      <a href={href}>
+        <div>
+          <img src={imgSrc} alt={title} />
+        </div>
+        <div>
+          <h2>{title}</h2>
+          <time dateTime={date}>{date}</time>
+        </div>
+      </a>
+    </li>
+  );
+};
+
+export default SubBlogCard;
