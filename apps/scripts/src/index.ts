0df0fa084d21a44061d2506bfe86129d8128cbf9
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-02-02 10:29:28 +0000

    bun runしやすくしました

--- apps/scripts/src/index.ts
+++ apps/scripts/src/index.ts
@@ -1,49 +1,17 @@
-import { ANSWER_PROMPT, SEARCH_PROMPT } from "./constants/prompt.js";
-import { GoogleSearchClient, type SearchResponse } from "./lib/agentBuilder.js";
+import { match } from "ts-pattern";
+import { projectTopix } from "./lib/project-topix/index.js";
 
-export interface Result {
-  document: {
-    id: string;
-  };
-  snippet?: string;
-}
+type ContentType = string | undefined;
 
-const searchClient = new GoogleSearchClient({
-  // biome-ignore lint/complexity/useLiteralKeys: .env
-  projectId: Bun.env["AGENT_BUILDER_PROJECT_ID"] || "",
-  // biome-ignore lint/complexity/useLiteralKeys: .env
-  location: Bun.env["AGENT_BUILDER_LOCATION"] || "global",
-  // biome-ignore lint/complexity/useLiteralKeys: .env
-  collectionId: Bun.env["AGENT_BUILDER_COLLECTION_ID"] || "",
-  // biome-ignore lint/complexity/useLiteralKeys: .env
-  engineId: Bun.env["AGENT_BUILDER_ENGINE_ID"] || "",
-});
-
+// 以下で個別に叩けます。引数無しで全て通しで叩く想定です
+// bun run ./src/index contentType(project-topix)
 const main = async () => {
-  const searchResults: SearchResponse =
-    await searchClient.search(SEARCH_PROMPT);
-
-  const answerResults: Result[] = searchResults.results.map((result) => ({
-    document: {
-      id: result.document.id,
-    },
-    snippet: result.document.derivedStructData.snippets[0]
-      ? result.document.derivedStructData.snippets[0].snippet
-      : "No Snippppet Available",
-  }));
-
-  const generatedAnswer = await searchClient.getGeneratedAnswer(
-    SEARCH_PROMPT,
-    ANSWER_PROMPT,
-    searchResults.queryId,
-    searchResults.session,
-  );
-
-  const result = {
-    searchResults: answerResults,
-    generatedAnswer: generatedAnswer.answer.answerText,
-  };
-  console.log(result);
+  const contentType: ContentType = Bun.argv[2];
+  match(contentType)
+    .with("project-topix", projectTopix)
+    .otherwise(() => {
+      projectTopix();
+    });
 };
 
 main();
