2ec7b6cbf89fdf965c5ed8ed3e940e209047bec0
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-02-08 07:41:23 +0000

    格言の生成

---
+++ apps/scripts/src/get-adage.ts
@@ -0,0 +1,9 @@
+import { fetch } from "./fetch/fetch-adage.js";
+import { parse } from "./ parse.js";
+import type { ContentType } from "./types.js";
+
+export const getAdage = async (): Promise<ContentType[]> => {
+  console.log("--- Adage ---");
+  const fetchData = await fetch();
+  return parse(fetchData);
+};
