8572d03c0467771a3cb5b985a2be11cc75bf40b8
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-02-08 18:13:13 +0000

    画像パスを整形できるようにした

--- apps/scripts/src/parse.ts
+++ apps/scripts/src/parse.ts
@@ -1,10 +1,10 @@
 import type { ContentType } from "./types.js";
 
 export const parse = (content: ContentType[]) => {
-  return content.map(({ title, content }) => ({
+  return content.map(({ title, content, image }) => ({
     title,
     content,
-    image: "",
+    image: image || "",
     links: [],
   }));
 };
