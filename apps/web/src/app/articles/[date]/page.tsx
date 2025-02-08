aa9745c368a8767e9b71bd218e54fa0927759a09
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-02-08 14:18:23 +0000

    新聞詳細ページのコンポネントを置き換えました

--- apps/web/src/app/articles/[date]/page.tsx
+++ apps/web/src/app/articles/[date]/page.tsx
@@ -1,6 +1,10 @@
+import { FirstParagraph } from "@/components/FirstParagraph";
+import { TopicTitle } from "@/components/TopicTitle";
 import { Header } from "@/components/layouts/Header/";
 import { prisma } from "@repo/database";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import SampleImage from "public/sample.webp";
 
 export const Page = async () => {
   const data = await prisma.article.findFirst({
@@ -15,20 +19,23 @@ export const Page = async () => {
       <Header />
 
       <main className="grid gap-y-6 mx-12">
-        <h1 className="font-bold text-2xl">title: {data.title}</h1>
         <p className="font-light text-sm">date: {data.date}</p>
-        {data.contents.map(({ title, content, image }) => {
-          return (
-            <div
-              className="shadow-lg bg-white p-4 grid gap-y-4 rounded"
-              key={title}
-            >
-              <h2 className="font-bold text-xl">title:{title}</h2>
-              <p>content:{content}</p>
-              <p>image url:{image ? image : "なし"}</p>
-            </div>
-          );
-        })}
+        <div className="grid gap-y-12">
+          {data.contents.map(({ title, content, image }) => {
+            return (
+              <div className="grid gap-y-6" key={title}>
+                <TopicTitle title={title} />
+                <Image
+                  src={image || SampleImage}
+                  alt={title || "画像"}
+                  height="448"
+                  width="896"
+                />
+                <FirstParagraph text={content} />
+              </div>
+            );
+          })}
+        </div>
       </main>
     </>
   );
