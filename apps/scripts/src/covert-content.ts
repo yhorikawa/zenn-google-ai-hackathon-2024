8f0037f8022582cc39f2cc0753b149650c40d5a7
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2025-02-08 07:42:20 +0000

    出力データを整形し、合体させる関数作成

--- apps/scripts/src/covert-content.ts
+++ apps/scripts/src/covert-content.ts
@@ -2,10 +2,10 @@ import type { ContentType } from "./types.js";
 
 export const convertContent = (
   content: ContentType[],
-  ...contents: ContentType[]
+  ...contents: ContentType[][]
 ) => {
   const convertedContent = contents.reduce((acc, currentContent) => {
-    acc.push(currentContent);
+    acc.push(...currentContent);
     return acc;
   }, content);
   return convertedContent;
