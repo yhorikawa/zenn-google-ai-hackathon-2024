a5b60e84076af68512329266626ed127429f6524
oka <76928095+takahiro-okada@users.noreply.github.com>
2025-02-10 08:55:47 +0000

    ✨add font fetching and apply "Shippori Mincho" to DefaultImage component

--- apps/web/src/app/api/v1/images/_components/Image.tsx
+++ apps/web/src/app/api/v1/images/_components/Image.tsx
@@ -6,12 +6,44 @@ type ImageResponse = {
   options: ImageResponseOptions;
 };
 
+export async function fetchFont(): Promise<ArrayBuffer | null> {
+  // biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
+  const googleFontsUrl = `https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@700&display=swap`;
+
+  const css = await (
+    await fetch(googleFontsUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+      },
+    })
+  ).text();
+
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/,
+  );
+
+  if (!resource) return null;
+  if (!resource[1]) return null;
+  const res = await fetch(resource[1]);
+  return res.arrayBuffer();
+}
+
 async function DefaultImage({
   text,
 }: { text: string }): Promise<ImageResponse> {
+  const fontData = await fetchFont();
+
   const options: ImageResponseOptions = {
     width: 1200,
     height: 630,
+    fonts: [
+      {
+        name: "Shippori Mincho",
+        data: fontData || new ArrayBuffer(0),
+        weight: 700,
+      },
+    ],
   };
 
   const element = (
@@ -23,6 +55,7 @@ async function DefaultImage({
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        fontFamily: "Shippori Mincho",
       }}
     >
       <img
@@ -41,18 +74,23 @@ async function DefaultImage({
           top: "14%",
           fontSize: "60px",
           fontWeight: "bold",
+          fontFamily: "Shippori Mincho",
         }}
       >
         今週の格言
       </p>
       <p
         style={{
-          position: "absolute",
-          top: "43%",
           width: "70%",
           wordWrap: "break-word",
           fontSize: "48px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          position: "absolute",
+          top: "40%",
           textAlign: "center",
+          fontFamily: "Shippori Mincho",
         }}
       >
         {text}
